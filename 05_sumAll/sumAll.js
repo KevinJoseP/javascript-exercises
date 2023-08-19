722f3e20b00c127e32056cb1d1b9f78a52ad074e
Kevin Jose <krsj1234@gmail.com>
2023-08-19 13:50:36 +0530

    sumall soln

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(st, en) {
+    if (typeof st != "number" || 
+        typeof en != "number" ||
+        st < 0 ||
+        en < 0)return "ERROR";
+    
+    let stn= st; 
+    let enn = en;
+
+    if (st > en)
+    {
+        stn = en;
+        enn = st;
+    }
+    let stSum = stn*(stn - 1);
+    stSum/=2;
+
+    let enSum = enn*(enn + 1);
+    enSum/=2;
+
+    return enSum - stSum;
 
 };
 
