5e0098fd4db807415e4e4398729852c98fac236c
Kevin Jose <krsj1234@gmail.com>
2023-08-19 13:37:06 +0530

    remove from array exercise soln

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let arr2 = [];
 
+    for (let i = 0; i < arr.length;i++)
+    {
+        if (!args.includes(arr[i]))
+        {
+            arr2.push(arr[i]);
+        }
+    }
+    return arr2;
 };
 
 // Do not edit below this line
