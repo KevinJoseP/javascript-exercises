1fc2a3ae90578d77e1863173624219df6ebebe82
Kevin Jose <krsj1234@gmail.com>
2023-08-30 00:12:10 +0530

    calculator soln

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(a) {
+  const sumd = a.reduce((sum, e) => sum + e, 0);
+  console.log(sumd);
+  return sumd;
 	
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  if (a.length == 0)return 0;
+  const pro = a.reduce((p, e) => p*e, 1);
+  console.log(pro);
+  return pro;
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if (a <= 1)return 1;
+  return a*factorial(a-1);
 	
 };
 
