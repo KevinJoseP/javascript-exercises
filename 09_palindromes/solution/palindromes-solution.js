2b54d39a94a5740e805cfbef254f7be20365000f
Kevin Jose <krsj1234@gmail.com>
2023-08-30 00:25:56 +0530

    palindrome soln

--- 09_palindromes/solution/palindromes-solution.js
+++ 09_palindromes/solution/palindromes-solution.js
@@ -1,5 +1,5 @@
-const palindromes = function (string) {
-  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+const palindromes = function (str) {
+  const processedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
   return processedString.split("").reverse().join("") == processedString;
 };
 
